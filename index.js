7952f7961c8ce9e0df8bf3e3d9646a6bd590a289
Farah Arar <farah02arar@gmail.com>
2023-10-05 08:02:28 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+
+const records = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"},
+    {year: "2015", result: "W"},
+
+    
+  ]
+
+  function superbowlWin(record) {
+    const winGame = record.find(game => game.result === "W");
+  
+    return winGame ? winGame.year : undefined;
+  }
